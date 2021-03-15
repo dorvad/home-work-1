69e3750495576d29021bae1b9366e868d9fb1ac6
orysia1903 <orysia.pirko991@gmail.com>
2021-03-15 21:34:33 +0200

    ccc

--- script.js
+++ script.js
@@ -2,26 +2,29 @@ function chooseFirstItem (){
     var firstItem = document.getElementById('methods').firstChild
     firstItem.setAttribute('style', 'color:blue')
 }
+
 function chooseLastItem (){
     var lastItem = document.getElementById('methods').lastChild
     lastItem.setAttribute('style', 'color:blue')
 }
+
 function chooseNextItem(){
-  
-   var nextItem = document.getElementsByClassName('next')[0];
-   nextItem = nextItem.nextSibling.setAttribute('style', 'color:blue')
-   var nextItem = document.getElementsByClassName('next')[1];
-   nextItem = nextItem.nextSibling.setAttribute('style', 'color:blue')
+    var next = document.getElementsByClassName('next');
+
+for(var i=0; i<next.length; i++){
+   var next = document.getElementsByClassName('next')[i];
+   next.nextSibling.innerHTML = next[i]
+}
     // треба щоб кожен елемент по порядку вибирав клік кнопки а вибирає всі разом
 }
-function chooseNextItem(){
-var next = document.getElementsByClassName('next')
-    for(var j=0; j<next.length; j++){
-        next[j].onclick = function (){
-            next.nextSibling.innerHTML = ('style', 'color:pink')
-        }
-    }
-}
+// function chooseNextItem(){
+// var next = document.getElementsByClassName('next')
+//     for(var j=0; j<next.length; j++){
+//         next[j].onclick = function (){
+//             next.nextSibling.innerHTML = ('style', 'color:pink')
+//         }
+//     }
+// }
 function appendText(form) {
     if (form.inputText.value) {
         var newItem = document.createElement("li");
@@ -29,12 +32,14 @@ function appendText(form) {
         document.getElementById("methods").appendChild(newItem);
     }
 }
+
 function restoreItem(){
     var restoreItems = document.getElementById('methods').lastChild
     if(restoreItems.parentNode){
         restoreItems.parentNode.removeChild(restoreItems)
     }
 }
+
 function appendBefore(form){
     if (form.inputText.value) {
         var newItem = document.createElement("li");
@@ -42,6 +47,5 @@ function appendBefore(form){
         var firstItem = document.getElementById("first")
         var parentFirstItem = firstItem.parentNode
         parentFirstItem.insertBefore(newItem, firstItem);
-
     }
 }
